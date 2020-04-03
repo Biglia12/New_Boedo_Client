18365c8c97da2ba66a557c8da1e205504aaa7a23
Ezequiel Biglia <ezequielbiglia12@gmail.com>
2020-04-02 23:59:49 -0300

    agego de infomacion antes del pago. agego de image view que lleva a la zona de maps donde maca los tipos de zona.

---
+++ app/src/main/java/com/example/androidfood/Infomacion/Aviso.java
@@ -0,0 +1,14 @@
+package com.example.androidfood.Infomacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Aviso extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aviso);
+    }
+}
